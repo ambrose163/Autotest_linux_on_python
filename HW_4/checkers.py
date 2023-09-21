7313d693bdefec55b804b749049a6c9fbb6cf68c
Dmitriy <ambrose163@mail.ru>
2023-09-21 17:42:39 +0400

    save

--- HW_4/checkers.py
+++ HW_4/checkers.py
@@ -1,12 +1,14 @@
 import subprocess
 
-
-def checkout(args, text):
-    res = subprocess.run(args, shell=True, stdout=subprocess.PIPE, encoding="utf-8")
-    if text in res.stdout and res.returncode == 0:
+def checkout(cmd, text):
+    result = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE, encoding='utf-8')
+    if text in result.stdout and result.returncode == 0:
         return True
     else:
         return False
+
+def getout(cmd):
+    return subprocess.run(cmd, shell=True, stdout=subprocess.PIPE, encoding='utf-8').stdout
 
 def checkout_negative(args, text):
     res = subprocess.run(args, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, encoding="utf-8")
